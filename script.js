727dd639e169054414f6f1bf04d5446eaa18808a
alexsgross <alexsgross@hotmail.com>
2022-07-13 09:11:07 -0300

    finalizando o projeto

---
+++ script.js
@@ -0,0 +1,24 @@
+const hourHand = document.querySelector('.hand.hours');
+const minutesHand = document.querySelector('.hand.minutes');
+const secondshand = document.querySelector('.hand.seconds');
+
+const setRotation = (element, rotationPercentage) => {
+    element.style.setProperty('--rotation', rotationPercentage * 360);
+};
+
+const setClock = () => {
+    const currentDate = new Date();
+
+    const secondsPercentage = currentDate.getSeconds() / 60;
+    const minutesPercentage = (secondsPercentage + currentDate.getMinutes()) / 60;
+    const hoursPercentage = (minutesPercentage + currentDate.getHours()) / 12;
+
+
+    setRotation(secondshand, secondsPercentage);
+    setRotation(minutesHand, minutesPercentage);
+    setRotation(hourHand, hoursPercentage);
+};
+
+setClock();
+
+setInterval(setClock, 1000);
